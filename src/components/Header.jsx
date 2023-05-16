57002a6a90ed1590dc300ef1f998dfed4dd3c9d7
Maverick Seneris <mavseneris@gmail.com>
2023-05-16 11:54:22 +0800

    applied initial header styles and layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,44 +1,62 @@
-import React, {useState, useEffect} from 'react'
-
+import React, { useState, useEffect } from 'react'
+import logo from "../assets/images/logo.png"
+import logoHouse from "../assets/images/logo-house.png"
 
 
 export default function Header() {
   const [date, setDate] = useState(new Date())
 
-  useEffect(()=>{
-    const interval = setInterval(()=>{
+  useEffect(() => {
+    const interval = setInterval(() => {
       setDate(new Date())
     }, 1000);
 
     return () => {
       clearInterval(interval)
     }
-  },[])
+  }, [])
 
-  const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thrusday", "Friday", "Saturday"]
-  const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+  function formatdate(date) {
+    const days = [
+      "Sunday", "Monday",
+      "Tuesday", "Wednesday",
+      "Thrusday", "Friday",
+      "Saturday"
+    ]
+    const months = [
+      "January", "February",
+      "March", "April",
+      "May", "June",
+      "July", "August",
+      "September", "October",
+      "November", "December"
+    ]
 
-  const dayOfWeek = days[date.getDay()]
-  const month = months[date.getMonth()]
-  const dayOfMonth = date.getDate()
-  const year = date.getFullYear()
+    const dayOfWeek = days[date.getDay()]
+    const month = months[date.getMonth()]
+    const dayOfMonth = date.getDate()
+    const year = date.getFullYear()
 
+    return `${dayOfWeek}, ${month} ${dayOfMonth}, ${year}`;
+  }
 
   return (
     <div className='header container'>
-      <span className='header-date'>{dayOfWeek}, {month} {dayOfMonth}, {year}</span>
+      <span className='header-date'>
+        {formatdate(date)}
+      </span>
       <div className='header--title'>
-        <div className='header-logo'>
-          <img src="#" />
+        <div className='header--logo'>
+          <img src={logo} />
         </div>
         <h1>FINE89 COFFEE</h1>
-        <div className='header-logo'>
-          <img src="#" />
+        <div className='header--logo'>
+          <img src={logo} />
         </div>
       </div>
       <div className='header--sub-title'>
         <span>ESTD . 2014</span>
-        <div><img src="#" /></div>
+        <div className='header--logo-house'><img src={logoHouse} /></div>
         <span>MANILA - PH</span>
       </div>
 
