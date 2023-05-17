d60dfe462256036910188d8adcb2092a685e4750
Maverick Seneris <mavseneris@gmail.com>
2023-05-17 10:00:14 +0800

    added some utility classes for repeating designs acrross the landing page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,13 +4,41 @@ import logoHouse from '../assets/images/logo-house.png'
 
 export default function Contact() {
   return (
-    <section className='contact' id='#contact' style={{border: "1px solid red"}}>
-      <div className='contact--section'style={{border: "1px solid red"}}>
-
+    <section className='contact' id='#contact' style={{ border: "1px solid red" }}>
+      <div className='section--divider'>
+        <div className='section--divider-line'></div>
+        <img className='section--divider-image'
+          src='https://res.cloudinary.com/dsbhrb9cr/image/upload/c_pad,b_auto:predominant,fl_preserve_transparency/v1684214620/FINE89%20Assets/watercolor-coffee-bean-1-300x235_o8fdce.jpg?_s=public-apps'
+        />
+        <div className='section--divider-line'></div>
       </div>
-      <div className='contact--section'style={{border: "1px solid red"}}>
 
+      <div className='contact--sub-sections'>
+        <div className='contact--section'>
+          <div className='section--title'>
+            <h2
+              className='section--title-header'
+            >
+              Opening Hours
+            </h2>
+          </div>
+
+        </div>
+        <div className='contact--section flex-end'>
+          <div className='section--title'>
+            <h2
+              className='section--title-header'
+            >
+              Contacts
+            </h2>
+            <img
+              src={logoHouse}
+              className='section--title-image'
+            />
+          </div>
+        </div>
       </div>
+
     </section>
   )
 }
--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -3,12 +3,13 @@ import logoHouse from '../assets/images/logo-house.png'
 
 export default function Reviews() {
   return (
-    <section className='reviews'>
+    <section className='reviews' id='reviews'>
       <div className='section--divider'>
         <div className='section--divider-line'></div>
         <img className='section--divider-image' src='https://res.cloudinary.com/dsbhrb9cr/image/upload/c_pad,b_auto:predominant,fl_preserve_transparency/v1684214620/FINE89%20Assets/watercolor-coffee-bean-1-300x235_o8fdce.jpg?_s=public-apps' />
         <div className='section--divider-line'></div>
       </div>
+      
       <div className='section--title flex-center'>
         <img
           src={logoHouse}
